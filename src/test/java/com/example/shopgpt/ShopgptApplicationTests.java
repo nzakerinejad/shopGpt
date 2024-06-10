efe727dd7de61cbe18c4d38e6d3d758d26af3a6b
Negar <n.zakerinejad@gmail.com>
2024-06-10 12:33:00 +0200

    first commit

---
+++ src/test/java/com/example/shopgpt/ShopgptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopgptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
